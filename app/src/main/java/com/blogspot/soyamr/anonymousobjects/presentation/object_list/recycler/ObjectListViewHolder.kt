028cd793dc753613074d0bbab3f48e7bbd14b5d3
Amr <Amr>
2021-05-23 20:19:07 +0700

    Added icons.

--- app/src/main/java/com/blogspot/soyamr/anonymousobjects/presentation/object_list/recycler/ObjectListViewHolder.kt
+++ app/src/main/java/com/blogspot/soyamr/anonymousobjects/presentation/object_list/recycler/ObjectListViewHolder.kt
@@ -1,9 +1,11 @@
 package com.blogspot.soyamr.anonymousobjects.presentation.object_list.recycler
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.blogspot.soyamr.anonymousobjects.R
 import com.blogspot.soyamr.anonymousobjects.databinding.ObjectItemBinding
 import com.blogspot.soyamr.domain.models.Object
 
@@ -13,12 +15,14 @@ class ObjectListViewHolder private constructor(private val binding: ObjectItemBi
     fun bind(item: Object, listener: (Int) -> Unit) {
         with(binding) {
             titleTextView.text = item.title
-            nameTextView.text = item.name + " - tag: " + item.tags
+            nameTextView.text = item.name
             if (!item.tags.isNullOrEmpty()) {
-                ratingBar.isVisible = true
-                ratingBar.rating = item.tags!!.size.toFloat()
-            } else {
-                ratingBar.isVisible = false
+                binding.statusHolder.removeAllViews()
+                for(i in item.tags!!.indices){
+                    View.inflate(binding.root.context, R.layout.cat_view, binding.statusHolder)
+                }
+            }else{
+                binding.statusHolder.removeAllViews()
             }
             root.setOnClickListener { listener(item.id) }
         }
